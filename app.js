0bfda80d90b5f7e39fe8ac189725589bbbf07504
Hans Huebner <hans.huebner@gmail.com>
2013-08-22 09:06:54 +0200

    proxy API requests to goeuro's server

--- app.js
+++ app.js
@@ -6,18 +6,33 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var proxy = require('http-proxy');
 
 var app = express();
+
+var apiHost = 'pre.dev.goeuro.de';
+var apiPort = 12345;
+
+var routingProxy = new proxy.RoutingProxy();
+ 
+function apiProxy(pattern, host, port) {
+    return function(req, res, next) {
+	if (req.url.match(pattern)) {
+	    routingProxy.proxyRequest(req, res, { host: host, port: port });
+	} else {
+	    next();
+	}
+    }
+}
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.errorHandler());
+app.use(apiProxy(new RegExp('/api/v1/.*'), apiHost, apiPort));
 
 app.get('/', function (req, res) {
     res.redirect('/index.html');
